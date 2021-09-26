975559349ba78fb8a06d4070695568b4fd90dc19
Isaque Freitas <freitasmelo.mkt.online@gmail.com>
2021-09-26 14:46:51 -0300

    Automatizei um pouco

--- script.js
+++ script.js
@@ -1,7 +1,12 @@
 
+let aluno = {
+    nome: "Isaque",
+    notas:[Number (prompt("Sua primeira nota")),Number (prompt("Sua segunda nota"))]
 
+}
 function notas(n1,n2){
-    return notas;
+    return (n1 + n2) / 2;
 }
 
-console.log(notas(10,20))
+let result = notas(aluno.notas[0],aluno.notas[1])
+console.log(parseFloat(result));
